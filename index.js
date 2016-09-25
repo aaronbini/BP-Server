7050e7bf0e67d0705fa6c787ff0834dc69cef650
Aaron Bini <bini.aaron@gmail.com>
2016-09-24 21:57:33 -0700

    more initial file setup

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const app = require('./lib/app');
+require( './lib/mongoose-setup' );
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log('Server running on', port);
