83da1824b985f87fa683677ec52abde0d4b7727e
Aaron Bini <bini.aaron@gmail.com>
2016-11-13 19:32:00 -0800

    trying again to edit before-after-test file

--- test/e2e/before-after-test.js
+++ test/e2e/before-after-test.js
@@ -1,9 +1,23 @@
-// start the db, and store connection, 
+//start db, store connection
 const connection = require('../../lib/mongoose-setup');
 
+//drop db before starting
+before( done => {
+  const drop = () => connection.db.dropDatabase(done);
+  if (connection.readyState === 1) drop();
+  else connection.once('open', drop);
+});
+
+//close connection after all tests run
+after(() => connection.close());
+
+
+// start the db, and store connection,
+// const connection = require( '../../lib/mongoose-setup' );
+
 // drop the database before starting:
-const db = require('./db');
-before(db.drop());
+// const db = require('./db');
+// before(db.drop());
 
 // close connection when done:
-after(() => connection.close());
+// after(() => connection.close());
