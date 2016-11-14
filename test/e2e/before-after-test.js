ba510e472ad01d6edf2156fa1cf56d39fe982190
Aaron Bini <bini.aaron@gmail.com>
2016-11-13 19:59:11 -0800

    still troubleshooting

--- test/e2e/before-after-test.js
+++ test/e2e/before-after-test.js
@@ -3,13 +3,17 @@ const connection = require('../../lib/mongoose-setup');
 
 //drop db before starting
 before( done => {
+  console.log('before all tests: ');
   const drop = () => connection.db.dropDatabase(done);
   if (connection.readyState === 1) drop();
   else connection.once('open', drop);
 });
 
 //close connection after all tests run
-after(() => connection.close());
+after(() => {
+  console.log('after all tests');
+  connection.close();
+});
 
 
 // start the db, and store connection,
