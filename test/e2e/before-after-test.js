dd85720ab7f60dea692a9cdbabd53a4a173b423a
Aaron Bini <bini.aaron@gmail.com>
2016-11-14 11:02:52 -0800

    added dotenv npm package

--- test/e2e/before-after-test.js
+++ test/e2e/before-after-test.js
@@ -3,7 +3,7 @@ const connection = require('../../lib/mongoose-setup');
 
 //drop db before starting
 before( done => {
-  console.log('before all tests: ');
+  console.log('before all tests');
   const drop = () => connection.db.dropDatabase(done);
   if (connection.readyState === 1) drop();
   else connection.once('open', drop);
@@ -14,14 +14,3 @@ after(() => {
   console.log('after all tests');
   connection.close();
 });
-
-
-// start the db, and store connection,
-// const connection = require( '../../lib/mongoose-setup' );
-
-// drop the database before starting:
-// const db = require('./db');
-// before(db.drop());
-
-// close connection when done:
-// after(() => connection.close());
