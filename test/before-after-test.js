65023ee2504d9dcd6d8932a1129af5fbd82a1b00
Aaron Bini <bini.aaron@gmail.com>
2016-11-14 17:26:21 -0800

    changed travis config

--- test/before-after-test.js
+++ test/before-after-test.js
@@ -1,6 +1,5 @@
 const path = require('path');
 require('dotenv').load({path: path.join(__dirname, '../.env.test')});
-//start db, store connection
 const connection = require('../lib/mongoose-setup');
 
 //drop db before starting
